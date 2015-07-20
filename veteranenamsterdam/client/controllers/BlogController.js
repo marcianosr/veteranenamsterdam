e270da4dc0f31c912056d2061041ae815c52a3b5
Marciano Schildmeijer <marciano_schildmeijer@live.nl>
2015-07-21 00:06:41 +0200

    attempt for getting blog images

--- veteranenamsterdam/client/controllers/BlogController.js
+++ veteranenamsterdam/client/controllers/BlogController.js
@@ -1,22 +1,29 @@
 angular.module('VA')
 .controller('BlogController', ['$scope', '$meteor', function($scope, $meteor) {
 
-	
+
   console.log('BlogController Init');
 
 
   $scope.posts = $meteor.collection(function(){
 
-  	console.log('get blog posts'); 
+  	console.log('get blog posts');
+
+
 
   	return Posts.find({ }, { })
-  }); 
+  });
 
    // $scope.images = $meteor.collectionFS(Images, false, Images).subscribe('images');
    // console.log($scope.images)
 
-  $scope.images = $meteor.collection(Images)
-  console.log($scope.images)
+  $scope.postImages = $meteor.collection(function() {
+
+			return Images.find({})
+	})
+	console.log($scope.postImages['0'])
+
+
 
 
 
@@ -27,10 +34,11 @@ angular.module('VA')
 Template.imageView.helpers({
   images: function () {
 
+	// var image_id =	Posts.find({image_id: true})
+	// console.log(image_id)
 
-  	return Images.find({_id: "xDJ3wuKSdHE5Frhhh"});
-    return Images.find(); // Where Images is an FS.Collection instance
+  	//  return Images.find({_id: image_id );
+		// console.log(Images.find().fetch()[0].url())
+    return Images.find({}, {}); // Where Images is an FS.Collection instance
   }
 });
-
-
--- veteranenamsterdam/client/controllers/CreatePostController.js
+++ veteranenamsterdam/client/controllers/CreatePostController.js
@@ -3,36 +3,33 @@ angular.module("VA").controller('CreatePostController', ['$rootScope', '$scope',
 
 		$scope.errorForm = true;
 		$scope.uploadImage = false;
+		$scope.post = {};
 
 		$scope.createPost = function() {
 
-			var post = $scope.validate(); 
+			var post = $scope.validate();
 			console.log(post)
 
-		 	
-			if(post && !$scope.errorForm) { 
+
+			if(post && !$scope.errorForm) {
 				console.log('succes')
 
 
 
-				console.log($scope.uploadImage)
 
-
-
-				if($scope.uploadImage) { 
+				if($scope.uploadImage) {
 					console.log(' upload a pic')
-
-					// post.image_id = 
-				} 
-				else { 
+					$scope.post.image_id = $scope.image_id;
+				}
+				else {
 					console.log('do not upload')
 				}
 
+				console.log($scope.post)
 
 
 
-
-				post._id = Posts.insert(post);
+				post._id = Posts.insert($scope.post);
 				$location.path('/blog')
 
 			}
@@ -40,117 +37,112 @@ angular.module("VA").controller('CreatePostController', ['$rootScope', '$scope',
 				console.log('not succes')
 
 			}
+		};
 
-			 
-		}; 
-
-		$scope.validate = function() { 
+		$scope.validate = function() {
 
 			$scope.errors = [];
 
-		   	var post = {
+		   	$scope.post = {
 		    	  title: $('input[name="title"]').val(),
 		    	  intro: $('input[name="intro"]').val(),
 				  message: $('textarea').val(),
 				  date: new Date(),
-				  author: $rootScope.currentUser.emails[0].address, 
-				  image_id: null,
+				  author: $rootScope.currentUser.emails[0].address,
+
 			};
 
-			if(post.title.length < 1) { 
+			if($scope.post.title.length < 1) {
 
 				$scope.errors.push('Het titel veld is leeg.');
 			}
 
-			if(post.message.length < 1) { 
+			if($scope.post.message.length < 1) {
 
 				$scope.errors.push('Het bericht veld is leeg.');
 
 			}
 
-			if(post.intro.length < 1) { 
+			if($scope.post.intro.length < 1) {
 
 				$scope.errors.push('Het intro veld is leeg.');
 
 			}
 
-			if($scope.errors.length != 0) { 
+			if($scope.errors.length != 0) {
 
 				$scope.errorForm = true;
 				return false;
 
 			}
-			else { 
+			else {
 				console.log('postting')
-				
-				
 				$scope.errorForm = false;
 
-				return post;
+				return $scope.post;
 
 			}
 
 		}
 
-		$scope.upload = function(files) { 
-
-			if (files && files.length) {
-	            for (var i = 0; i < files.length; i++) {
-	                var file = files[i];
-	                console.log(file)
-	                Upload.upload({
-	                    url: '/uploads',
-	                    // fields: {
-	                    //     'username': $scope.username
-	                    // },
-	                    file: files
-	                }).progress(function (evt) {
-	                    var progressPercentage = parseInt(100.0 * evt.loaded / evt.total);
-	                    $scope.log = 'progress: ' + progressPercentage + '% ' +
-	                                evt.config.file.name + '\n' + $scope.log;
-	                }).success(function (data, status, headers, config) {
-	                    $timeout(function() {
-	                        $scope.log = 'file: ' + config.file.name + ', Response: ' + JSON.stringify(data) + '\n' + $scope.log;
-	                    });
-	                });
-	            }
-	        }
-	}
 
 
-	$scope.uploadFile = function(event) { 
+	$scope.uploadFile = function(event) {
 		// console.log(FS)
 		// console.log(event)
 
-		// var file = event.target.files; 
+		// var file = event.target.files;
 
 		 // console.log(event.target.files.length)
-		// if(event == undefined) 
-		// { 
+		// if(event == undefined)
+		// {
 		// 	$scope.upload = false;
 		// 	return false
 		// }
+
 			console.log('reach meee')
 			FS.Utility.eachFile(event, function(file) {
 	      		Images.insert(file, function (err, fileObj) {
 		        	// Inserted new doc with ID fileObj._id, and kicked off the data upload using HTTP
 
 		        	console.log('insert')
-		        	console.log(err)
+							console.log(err)
 		        	console.log(fileObj)
+		        	console.log(event.target.files)
+
+
+							$scope.image_id = fileObj._id;
 		     	});
 	     	 });
 
 			$scope.uploadImage = true;
-
-
+			// $scope.post.image_id = event.target.files[0]._id;
 	}
 
-
-
-		
 }]);
 
 
-
-
+// $scope.upload = function(files) {
+//
+// 	if (files && files.length) {
+// 					for (var i = 0; i < files.length; i++) {
+// 							var file = files[i];
+// 							console.log(file)
+// 							Upload.upload({
+// 									url: '/uploads',
+// 									// fields: {
+// 									//     'username': $scope.username
+// 									// },
+// 									file: files
+// 							}).progress(function (evt) {
+// 									var progressPercentage = parseInt(100.0 * evt.loaded / evt.total);
+// 									$scope.log = 'progress: ' + progressPercentage + '% ' +
+// 															evt.config.file.name + '\n' + $scope.log;
+// 							}).success(function (data, status, headers, config) {
+// 									$timeout(function() {
+// 											$scope.log = 'file: ' + config.file.name + ', Response: ' + JSON.stringify(data) + '\n' + $scope.log;
+// 									});
+// 							});
+// 					}
+// 			}
+// }
